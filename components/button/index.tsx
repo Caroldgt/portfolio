d2f3579384397c30efe388f39bc8250b733667c1
carol <caroldaga2@gmail.com>
2023-08-08 15:57:55 -0300

    projeto

---
+++ components/button/index.tsx
@@ -0,0 +1,15 @@
+
+import Link from "next/link"
+import styles from "./styles.module.css"
+
+function Button({ text = "", link = "" }) {
+    return (
+        <div className={styles.button}>S
+            <Link href={link} className={styles.buttonlink} target="_blank"> 
+                <span className={styles.buttonborder}>{text}</span>
+            </Link>
+        </div>
+    )
+}
+
+export default Button
